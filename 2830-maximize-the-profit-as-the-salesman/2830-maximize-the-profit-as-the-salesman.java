f14b6b1deafee5095c1da02e3879eaa2435b56c7
JongKyun Seon <37062147+SeonJK@users.noreply.github.com>
2024-02-04 22:29:57 +0900

    Time: 77 ms (66.98%), Space: 96.5 MB (47.17%) - LeetHub

---
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.java
@@ -0,0 +1,23 @@
+// answer = https://leetcode.com/problems/maximize-the-profit-as-the-salesman/solutions/3935072/java-easy-solution-dp-o-n-offers-comments-added/
+class Solution {
+		public int maximizeTheProfit(int n, List<List<Integer>> offers) {
+			Collections.sort(offers,(a,b)->a.get(1).compareTo(b.get(1)));
+
+            int[] ans=new int[n];
+            int curr=0;
+
+            for(int i=0;i<n;i++){
+                ans[i]=(i>0) ? ans[i-1]:0 ;//for calculating max and negating 0 index
+
+                //traversing the offers
+                while(curr<offers.size() && offers.get(curr).get(1)==i){
+                    int start=offers.get(curr).get(0);
+                    int gold=offers.get(curr).get(2);
+
+                    ans[i]=Math.max(ans[i],(start>0 ? ans[start-1]:0)+gold);
+                    curr++;
+                }
+            }
+        return ans[n-1];//returning max value
+		}
+	}
